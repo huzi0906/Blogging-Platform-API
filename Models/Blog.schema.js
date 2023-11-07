5dce03ecc675dad7b51fdd118704ec59b547651e
huzaifa <mhuzaifaahmad@gmail.com>
2023-11-08 00:02:51 +0500

    Add blog schema

---
+++ Models/Blog.schema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.model("Blog", blogSchema);
+module.exports = model;
