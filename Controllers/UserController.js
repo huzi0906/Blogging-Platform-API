cdde0e38a4f0137e5ac21b86f6bf3f9fe0c0666f
huzaifa <mhuzaifaahmad@gmail.com>
2023-11-08 09:32:27 +0500

    Add user registration functionality and fix user router import

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,7 +1,18 @@
 const user = require("../Models/User.schema");
 const jwt = require("jsonwebtoken");
 
-let register = async (req, res) => {};
+let register = async (req, res) => {
+  let { username, email, isAdmin, password } = req.body;
+  user
+    .create({ username, email, isAdmin, password })
+    .then(data => {
+      res.status(200).json({ Message: "User Created", data: data });
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
+
 let login = async (req, res) => {};
 let logout = async (req, res) => {};
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const UserRouter = require("./Routes/UserRouter");
+const UserRouter = require("./Routes/UserRoutes.js");
 
 const app = express();
 app.use(express.json());
