461dab4115653b3a8e9ff7325a97680bf1f0aa07
huzaifa <mhuzaifaahmad@gmail.com>
2023-11-10 02:44:40 +0500

    Add blog viewing functionality

--- Controllers/BlogController.js
+++ Controllers/BlogController.js
@@ -14,8 +14,20 @@ let createBlog = async (req, res) => {
     });
 };
 
-let viewBlog = async (req, res) => {};
+let viewBlog = async (req, res) => {
+  let { id } = req.params;
+  blog
+    .findById(id)
+    .then(data => {
+      res.status(200).json({ Message: "Blog Found", data: data });
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
+
 let updateBlog = async (req, res) => {};
+
 let deleteBlog = async (req, res) => {};
 
 let comment = async (req, res) => {};
