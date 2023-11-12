ed6433c31e7306a17f33e3cf839e94a9775c95ad
huzaifa <mhuzaifaahmad@gmail.com>
2023-11-12 18:28:30 +0500

    Add user following functionality

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -29,8 +29,54 @@ let updateProfile = async (req, res) => {
   }
 };
 
-let follow = async (req, res) => {};
+let follow = async (req, res) => {
+  let { id } = req.params;
+  let { signedInUser } = req.body;
+  if (id === signedInUser.id) {
+    res.status(403).json({ Message: "You Cannot Follow Yourself" });
+  } else {
+    user
+      .findOne({ _id: signedInUser.id, following: { $in: [id] } })
+      .then(data => {
+        if (data) {
+          res.status(400).json({ Message: "Already Followed" });
+        } else {
+          user
+            .updateOne(
+              { _id: id },
+              {
+                $push: {
+                  followers: signedInUser.id,
+                  notifications: `${signedInUser.username} started following you`,
+                },
+              }
+            )
+            .then(data => {
+              user
+                .updateOne(
+                  { _id: signedInUser.id },
+                  { $push: { following: id } }
+                )
+                .then(data => {
+                  res.status(200).json({ Message: "Followed" });
+                })
+                .catch(err => {
+                  res.status(500).send(err);
+                });
+            })
+            .catch(err => {
+              res.status(500).send(err);
+            });
+        }
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  }
+};
+
 let unfollow = async (req, res) => {};
+
 let viewNotifications = async (req, res) => {};
 
 module.exports = {
