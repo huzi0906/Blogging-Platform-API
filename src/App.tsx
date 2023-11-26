661e94863d78aebbe228269c38f43c1d24744d0f
huzaifa <mhuzaifaahmad@gmail.com>
2023-11-26 23:58:09 +0500

    Implement Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import LandingPage from "./components/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import LandingPage from "./pages/LandingPage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    children: [],
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "signup",
+    element: <Signup />,
+  },
+]);
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <LandingPage />
-    </>
+    <RouterProvider
+      router={router}
+      // fallbackElement={<LandingPage />}
+    />
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App";
+import Navbar from "./components/Navbar";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+
+    // primary: {
+    //   main: "#1976d2",
+    // },
+    // secondary: {
+    //   main: "#dc004e",
+    // },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+});
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <>
+          <Navbar />
+          <App />
+          <ToastContainer />
+        </>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
